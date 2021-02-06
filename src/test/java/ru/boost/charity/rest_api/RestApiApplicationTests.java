b3708a63b2e28d292e91a1724743b7e5a3e9e814
Rinchin Iakovlev <rbyakovlev@alfabank.ru>
2021-02-06 14:51:22 +0500

    Initial commit

---
+++ src/test/java/ru/boost/charity/rest_api/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.boost.charity.rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
